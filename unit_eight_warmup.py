58f2c27362c4af21fb3a0b74da4bd15775c61ca0
Frank <frank@iMac3.local>
2018-11-08 12:26:23 -0500

    Add variables

--- unit_eight_warmup.py
+++ unit_eight_warmup.py
@@ -2,16 +2,20 @@ import tkinter
 
 root = tkinter.Tk()
 
+fahrenheit = tkinter.StringVar()
+
+celsius = tkinter.StringVar()
+
 label1 = tkinter.Label(root, text="degrees F:")
 label1.grid(row=1, column=1)
 
-label2 = tkinter.Entry(root)
+label2 = tkinter.Entry(root, textvariable=fahrenheit)
 label2.grid(row=1, column=2)
 
 label3 = tkinter.Label(root, text="degrees C:")
 label3.grid(row=2, column=1)
 
-label4 = tkinter.Label(root)
+label4 = tkinter.Label(root, textvariable=celsius)
 label4.grid(row=2, column=2)
 
 root.mainloop()
