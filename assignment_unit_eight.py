6ced07411ab593fcb5969a9e1df073312543f222
Frank <frank@iMac3.local>
2018-11-08 12:47:53 -0500

    finish the sketch and begin the project

---
+++ assignment_unit_eight.py
@@ -0,0 +1,15 @@
+import tkinter
+
+root = tkinter.Tk()
+
+labelcalculator = tkinter.Label(root, text="calculator")
+labelcalculator.grid(row=1)
+
+labelEntry = tkinter.Entry(root)
+labelEntry.grid(row=2)
+
+label1 = tkinter.Label(root, text="1")
+
+label2 = tkinter.Label(root, text="2")
+
+label3 = tkinter.Label(root, text="3")
