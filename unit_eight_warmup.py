338160d5ef0716d3a1c0224cb3ae18b576129a6c
Frank <frank@iMac3.local>
2018-11-12 08:24:49 -0500

    creat a function to convert from F to C

--- unit_eight_warmup.py
+++ unit_eight_warmup.py
@@ -1,9 +1,16 @@
 import tkinter
 
 root = tkinter.Tk()
+root.title("Temperature Converter")
+
+
+def f_to_c():
+    f_value = int(fahrenheit.get())
+    c_value = 5/9 * (f_value - 32)
+    celsius.set(str(c_value))
+
 
 fahrenheit = tkinter.StringVar()
-
 celsius = tkinter.StringVar()
 
 label1 = tkinter.Label(root, text="degrees F:")
@@ -17,5 +24,10 @@ label3.grid(row=2, column=1)
 
 label4 = tkinter.Label(root, textvariable=celsius)
 label4.grid(row=2, column=2)
+
+
+convert_button = tkinter.Button(root, text="Convert", command=f_to_c)
+convert_button.grid(row=3, column=1)
+
 
 root.mainloop()
