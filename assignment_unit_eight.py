d776852ce9e1ff15789abdd28352c77ed9b491da
Frank <frank@iMac3.local>
2018-11-14 11:09:32 -0500

    final draft

--- assignment_unit_eight.py
+++ assignment_unit_eight.py
@@ -3,7 +3,6 @@
 # I finally make my calculator work by using a special function to "equal" which is "eval", and I finish my calculator.
 
 
-
 import tkinter
 
 root = tkinter.Tk()
@@ -109,59 +108,59 @@ def point():
 labelEntry = tkinter.Entry(root, textvariable=answer)
 labelEntry.grid(row=2, column=1, columnspan=4)
 
-label1 = tkinter.Button(root, text="1", command=one)
+label1 = tkinter.Button(root, text="   1   ", command=one)
 label1.grid(row=6, column=1)
 
 
-label2 = tkinter.Button(root, text="2", command=two)
+label2 = tkinter.Button(root, text="   2   ", command=two)
 label2.grid(row=6, column=2)
 
-label3 = tkinter.Button(root, text="3", command=three)
+label3 = tkinter.Button(root, text="   3   ", command=three)
 label3.grid(row=6, column=3)
 
-label4 = tkinter.Button(root, text="4", command=four)
+label4 = tkinter.Button(root, text="   4   ", command=four)
 label4.grid(row=5, column=1)
 
-label5 = tkinter.Button(root, text="5", command=five)
+label5 = tkinter.Button(root, text="   5   ", command=five)
 label5.grid(row=5, column=2)
 
-label6 = tkinter.Button(root, text="6", command=six)
+label6 = tkinter.Button(root, text="   6   ", command=six)
 label6.grid(row=5, column=3)
 
-label7 = tkinter.Button(root, text="7", command=seven)
+label7 = tkinter.Button(root, text="   7   ", command=seven)
 label7.grid(row=4, column=1)
 
-label8 = tkinter.Button(root, text="8", command=eight)
+label8 = tkinter.Button(root, text="   8   ", command=eight)
 label8.grid(row=4, column=2)
 
-label9 = tkinter.Button(root, text="9", command=nine)
+label9 = tkinter.Button(root, text="   9   ", command=nine)
 label9.grid(row=4, column=3)
 
-label0 = tkinter.Button(root, text="0", command=zero)
+label0 = tkinter.Button(root, text="       0       ", command=zero)
 label0.grid(row=7, column=1, columnspan=2)
 
-label_clear = tkinter.Button(root, text="C", command=clear)
+label_clear = tkinter.Button(root, text="   C   ", command=clear)
 label_clear.grid(row=3, column=1)
 
-label_equal = tkinter.Button(root, text="=", command=equal)
+label_equal = tkinter.Button(root, text="   =   ", command=equal)
 label_equal.grid(row=3, column=2)
 
-label_divide = tkinter.Button(root, text="/", command=divide)
+label_divide = tkinter.Button(root, text="   /   ", command=divide)
 label_divide.grid(row=3, column=3)
 
-label_multiply = tkinter.Button(root, text="*", command=multiply)
+label_multiply = tkinter.Button(root, text="   *   ", command=multiply)
 label_multiply.grid(row=3, column=4)
 
-label_minus = tkinter.Button(root, text="-", command=minus)
+label_minus = tkinter.Button(root, text="   -   ", command=minus)
 label_minus.grid(row=4, column=4)
 
-label_plus = tkinter.Button(root, text="+", command=plus)
+label_plus = tkinter.Button(root, text="   +   ", command=plus)
 label_plus.grid(row=5, column=4)
 
 label_enter = tkinter.Button(root, text="enter", command=enter)
 label_enter.grid(row=6, rowspan=2, column=4)
 
-label_point = tkinter.Button(root, text=".", command=point)
+label_point = tkinter.Button(root, text="   .   ", command=point)
 label_point.grid(row=7, column=3)
 
 root.mainloop()
